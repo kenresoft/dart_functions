0a35ae69b853888f90f0d75be719c250119fff71
Kenneth Amadi <amadikenneth12@yahoo.com>
2023-06-03 20:06:48 +0100

    Initial commit.

---
+++ bin/dart_functions.dart
@@ -0,0 +1,5 @@
+import 'package:dart_functions/dart_functions.dart' as dart_functions;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_functions.calculate()}!');
+}
---
+++ test/dart_functions_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_functions/dart_functions.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
