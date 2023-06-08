b4061fa39a84ed2572976dab88845ff621f0a245
Kenneth Amadi <amadikenneth12@yahoo.com>
2023-06-08 05:45:59 +0100

    Add btree.dart, hybrid_map.dart, and hybrid_map_2.dart to obtain efficient maps in dart.

--- bin/dart_functions.dart
+++ bin/dart_functions.dart
@@ -1,5 +1,8 @@
 import 'package:dart_functions/dart_functions.dart';
+import 'package:dart_functions/src/ml/recommendation.dart';
 import 'package:dart_functions/src/space.dart';
+
+import 'package:dart_functions/src/ml/forecast.dart';
 
 void main(List<String> arguments) {
   arguments = ['Multiply', 'Dart', 'Function'];
@@ -9,11 +12,27 @@ void main(List<String> arguments) {
 
   final object = list.mapFromList_1();
 
-  var ob = ["foo", "bar", "baz"].joinFix_2(separator: " ", prefix: "_", suffix: "_", applyToEach: true);
+  var ob = ["Welcome", "to", "Dart"].joinFix_2(separator: " ", prefix: "_", suffix: "_", applyToEach: true);
 
-  print(ob);
+  //print(ob);
 
   print('');
 
-  print(spacedDigits('4547548', 3));
+  //print(spacedDigits('4547548', 3));
+
+/*  List dataset = [
+    [2011, 80],
+    [2012, 90],
+    [2013, 92],
+    [2014, 83],
+    [2015, 94],
+    [2016, 99],
+    [2017, 92]
+  ];
+
+  print(straightLineForecast(2017, dataset));*/
+  var r = Recommendation();
+  print(r.dataset);
+  print('');
+  print(r.recommend());
 }
