f05f6c3ce44d207f7d8f5e0a4fe8345aca47b8e1
Kenneth Amadi <amadikenneth12@yahoo.com>
2023-06-11 06:09:30 +0100

    Add the equatable library to dependency

--- test/dart_functions_test.dart
+++ test/dart_functions_test.dart
@@ -2,7 +2,6 @@ import 'package:dart_functions/dart_functions.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('test', () {
   });
 }
